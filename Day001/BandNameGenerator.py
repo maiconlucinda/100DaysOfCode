459be41d63ad286dc48238cd11f0e8c9fe13bbfd
Maicon Lucinda <52203372+maiconlucinda@users.noreply.github.com>
2026-07-28 17:48:47 +0100

    Create BandNameGenerator.py

---
+++ Day001/BandNameGenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+
+name_of_city = input("What is the name of the city you grew up in? \n")
+name_pf_pet = input("What is the name of your pet? \n")
+
+print("Your band name could be " + name_of_city + " " + name_pf_pet)
